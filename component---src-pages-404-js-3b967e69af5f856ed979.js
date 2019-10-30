438afaaee67d7d7d7b8be81b838babf476721516
Jesus Rojas <jesusrojas.frontend@gmail.com>
2019-10-30 03:48:38 +0000

    Se hizo el blog

---
+++ component---src-pages-404-js-3b967e69af5f856ed979.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{246:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a),l=n(173),o=n(244);t.default=function(){return u.a.createElement(l.default,null,u.a.createElement(o.a,{title:"404: Not found"}),u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-3b967e69af5f856ed979.js.map
